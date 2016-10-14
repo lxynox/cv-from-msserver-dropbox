e80aad2b53375b9a54069e37da07f664e13d7853
lxynox <lxybitren@live.com>
2016-10-14 17:29:38 -0400

    unify indention across js files

--- bootstrap.js
+++ bootstrap.js
@@ -2,30 +2,30 @@ var fs = require('fs');
 
 // default path for cv.docx, cv.pdf and remote Dropbox sync folder
 const path = {
-	src: 'R:\\CV.docx',
-	pdf: 'R:\\CV.pdf',
- dest: '/Job/resume/'
+  src: 'R:\\CV.docx',
+  pdf: 'R:\\CV.pdf',
+  dest: '/Job/resume/'
 };
 
 fs.writeFile('path.json', JSON.stringify(path, null, '\t'), (err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log('path.json generated!');
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('path.json generated!');
 });
 
 fs.readFile('startup.bat', (err, data) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-	fs.writeFile(process.env.APPDATA + '\\Microsoft\\Windows\\Start\ Menu\\Programs\\Startup\\startup.bat', data, (err) => {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log(`startup.bat created in ${process.env.USERNAME} startup folder!`);
-	});
+  fs.writeFile(process.env.APPDATA + '\\Microsoft\\Windows\\Start\ Menu\\Programs\\Startup\\startup.bat', data, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`startup.bat created in ${process.env.USERNAME} startup folder!`);
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,44 @@
 var gulp = require('gulp');
- 
+
 var Dropbox = require('dropbox');
 var Promise = require('bluebird');
 var readFile = Promise.promisify(require('fs').readFile);
 var exec = require('child_process').exec;
- 
+
 const path = {};
- 
+
 // Dropbox acess
 const token='iernAqGWD_kAAAAAAAAe19plIWkCymKPuYd8Ll5GG82Y_H62baRYouaA8gOWfpYY';
 const dbx = new Dropbox({ accessToken: token });
- 
+
 // Tasks
 gulp.task('path', (done) => {
 
   readFile('path.json')
-	  .then(content => {
-			const config = JSON.parse(content);
-			Object.assign(path, config, {});
+    .then(content => {
+      const config = JSON.parse(content);
+      Object.assign(path, config, {});
 
-			done();
-		})
+      done();
+    })
     .catch(err => {
-			console.log(err);
-		});
+      console.log(err);
+    });
 
 });
 
 gulp.task('pdf', (done) => {
 
-	exec(`cscript saveAsPDF.js ${path.src} ${path.pdf}`, (err, stdout, stderr) => {
-		if (err) {
-			console.error(`exec error: ${error}`);
-			return;
-		}
+  exec(`cscript saveAsPDF.js ${path.src} ${path.pdf}`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
 
-		console.log(`\nPDF saved to: ${path.pdf}\n`);
-		done();
-	});
-  
+    console.log(`\nPDF saved to: ${path.pdf}\n`);
+    done();
+  });
+
 });
 
 gulp.task('sync', ['pdf'], () => {
@@ -47,10 +47,10 @@ gulp.task('sync', ['pdf'], () => {
     .then(content => {
       return dbx.filesUpload({
         contents: content, 
-				path: path.dest + 'CV.docx',
+        path: path.dest + 'CV.docx',
         mode: {
-	  '.tag': 'overwrite'
-	},
+          '.tag': 'overwrite'
+        },
         autorename: false
       });
     })
@@ -62,10 +62,10 @@ gulp.task('sync', ['pdf'], () => {
     .then(content => {
       return dbx.filesUpload({
         contents: content, 
-				path: path.dest + 'CV.pdf',
+        path: path.dest + 'CV.pdf',
         mode: {
-	  '.tag': 'overwrite'
-	},
+          '.tag': 'overwrite'
+        },
         autorename: false
       });
     })
@@ -74,10 +74,10 @@ gulp.task('sync', ['pdf'], () => {
     });
 
 });
- 
+
 gulp.task('watch', ['path'],  () => {
-	console.log(`\n?? Watching source: ${path.src}\n`);
+  console.log(`\n?? Watching source: ${path.src}\n`);
   gulp.watch(path.src, ['sync']);
 });
- 
+
 gulp.task('default', ['watch']);
